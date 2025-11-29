ddf6564c08e95c7168a44398cf4722abe7af8a25
GreeshKolla123 <2100030262@kluniversity.in>
2025-11-29 12:23:57 +0530

    🚀 Initial commit - Generated with CODE AGENT

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      <nav className="nav">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
